b5c5d9df2732159fa28b424f54f270482958b1eb
tlips <thomas.lips@ugent.be>
2022-11-11 21:55:17 +0100

    helper function for transforming points

--- airo_core/type_aliases.py
+++ airo_core/type_aliases.py
@@ -1,11 +1,17 @@
-from typing import Tuple
+from typing import Tuple, Union
 
 import numpy as np
 
 # TODO: see if we can specify the shape of these types for mypy
 
 VectorType = np.ndarray
-""" 3D vector that represents a position/translation/direction in cartesian space
+""" a (3,) np array that represents a 3D position/translation/direction
+"""
+BagOfPointsType = np.ndarray
+""" a (N,3) np array that represents N 3D points
+"""
+PointsType = Union[VectorType, BagOfPointsType]
+""" a convenience type that represents (3,) or (N,3) arrays of points.
 """
 QuaternionType = np.ndarray
 """scalar-last quaternion that represents a rotation around the <x,y,z> axis with angle <theta>
