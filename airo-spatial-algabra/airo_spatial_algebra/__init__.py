48659ecca19a2d7bf16f3ee5c1159d7cc6a67185
tlips <thomas17.lips@gmail.com>
2022-12-22 12:17:43 +0100

    update CI to run on each package separately
    commit e85053897e3a1b3506bd06dae83547bd6de62190
    Author: tlips <thomas17.lips@gmail.com>
    Date:   Thu Dec 22 12:11:58 2022 +0100
    
        mypy exclude pip build folder and fix pytest runs-on
    
    commit b30b71053f59088f74ca31c99df219bd03405b12
    Author: tlips <thomas17.lips@gmail.com>
    Date:   Thu Dec 22 11:53:20 2022 +0100
    
        mypy checking
    
    commit 3ba108d4a9782addb4b46febcae548edef7a4789
    Author: tlips <thomas17.lips@gmail.com>
    Date:   Thu Dec 22 11:45:42 2022 +0100
    
        fix typo in folder name
    
    commit e8d9369172b9426af80611b3003cb97b8f81c876
    Author: tlips <thomas17.lips@gmail.com>
    Date:   Thu Dec 22 11:43:47 2022 +0100
    
        try to install package
    
    commit c9837683b1adfb83008c21d530de2120662f2268
    Author: tlips <thomas17.lips@gmail.com>
    Date:   Thu Dec 22 11:38:50 2022 +0100
    
        yet another typo
    
    commit 63a90b00545665ebd1998bfecd97f0b433eefd60
    Author: tlips <thomas17.lips@gmail.com>
    Date:   Thu Dec 22 11:37:35 2022 +0100
    
        fix typo
    
    commit 88a949cbb82d114cf43ed71126bceb43402893e3
    Author: tlips <thomas17.lips@gmail.com>
    Date:   Thu Dec 22 11:36:15 2022 +0100
    
        unittest pipeline per package

--- airo-spatial-algabra/airo_spatial_algebra/__init__.py
+++
@@ -1,4 +0,0 @@
-from operations import transform_points
-from se3 import SE3Container
-
-__all__ = [SE3Container, transform_points]
---
+++ airo-spatial-algebra/airo_spatial_algebra/__init__.py
@@ -0,0 +1,4 @@
+from airo_spatial_algebra.operations import transform_points
+from airo_spatial_algebra.se3 import SE3Container
+
+__all__ = ["SE3Container", "transform_points"]
