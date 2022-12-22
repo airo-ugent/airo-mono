a44b47ba5f23e3de37a2edfc07672a4ca3b85b22
tlips <thomas17.lips@gmail.com>
2022-12-22 12:56:47 +0100

    bad attempt to include local dependencies - would copy the code and create multiple installs?

--- airo-spatial-algebra/setup.py
+++ airo-spatial-algebra/setup.py
@@ -1,5 +1,8 @@
 import setuptools
 from setuptools import find_packages
+import pathlib
+import os
+root_folder = pathlib.Path(__file__).parents[1]
 
 setuptools.setup(
     name="airo_spatial_algebra",
@@ -8,6 +11,7 @@ setuptools.setup(
     author="Thomas Lips",
     author_email="thomas.lips@ugent.be",
     install_requires=["numpy", "scipy", "spatialmath-python"],
-    packages=find_packages(),
+    packages=["airo_spatial_algebra", "airo_typing"],
+    package_dir={"airo_typing": os.path.join(root_folder, "airo-typing","airo_typing")},
     package_data={"airo_spatial_algebra": ["py.typed"]},
 )
