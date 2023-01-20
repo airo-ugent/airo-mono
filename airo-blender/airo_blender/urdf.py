cfc7a69d64838b7eab8f41c2e984a372d037b6a4
victorlouisdg <victorlouisdg@gmail.com>
2023-01-20 10:17:56 +0100

    finish urdf importer refactor

--- airo-blender/airo_blender/urdf.py
+++ airo-blender/airo_blender/urdf.py
@@ -3,17 +3,17 @@
 Features:
 - Only relative or absolute paths are supported in the URDF files, so not paths with //:package.
 - Only imports .dae, .stl and .obj mesh files.
-- Only joints that align with its parents axes are supported .
 - The imported URDFs are not rigged, so they must be posed in a "forwards kinematics" manner.
-- We return a dictionary with info over the free DoFs of the URDF, so that we can use it to generate random poses.
+- Joint limits are ignored.
+- We (will) return a dictionary with info over the free DoFs of the URDF, so that we can use it to generate random poses.
 
 Implementation:
 - We use xmltodict to parse the URDF file to a python dictionary.
   Child xml elements can be accessed from the dictionary by using the tag as key e.g. urdf_dict["robot"].
   Attributes can be accessed by using the "@" prefix e.g. link["@name"].
-- Then we import all links as empty objects. The visual meshes are imported and set as children of the empties.
+- Then we import all links as empty objects. The visual meshes are imported and set as children of the link_empties_by_name.
 - By default we lock all degrees of freedom of the links.
-- Joints are then parsed, which unlock degrees of freedom of their child link.
+- Joints are then parsed. TODO explain.
 """
 import os
 
@@ -34,14 +34,7 @@ def read_urdf_as_dictionary(urdf_path: str):
     return urdf_dict
 
 
-def create_empty_for_link(link: dict):
-    bpy.ops.object.empty_add(type="ARROWS", radius=0.05)
-    empty = bpy.context.object
-    empty.name = link["@name"]
-    return empty
-
-
-def set_transform_from_origin(object: bpy.types.Object, origin: dict):
+def set_delta_transform_from_origin(object: bpy.types.Object, origin: dict):
     if "@xyz" in origin:
         object.delta_location = parse_vector_string(origin["@xyz"])
     if "@rpy" in origin:
@@ -102,26 +95,29 @@ def import_visual(visual: dict, parent: bpy.types.Object, urdf_dir: str) -> None
         geometry_objects = import_mesh_from_urdf(mesh_path)
         if "@scale" in geometry["mesh"]:
             scales = parse_vector_string(geometry["mesh"]["@scale"])
+            bpy.ops.object.select_all(action="DESELECT")
             for object in geometry_objects:
                 object.scale = scales
-            bpy.ops.object.transform_apply(
-                {"selected_editable_objects": geometry_objects}, location=False, rotation=False, scale=True
-            )
+                object.location = (0, 0, 0)
+                object.rotation_euler = (0, 0, 0)
+                object.select_set(True)
+            bpy.ops.object.transform_apply()
 
     if "box" in geometry:
         bpy.ops.mesh.primitive_cube_add()
         box = bpy.context.object
         scales = parse_vector_string(geometry["box"]["@size"])
         box.scale = 0.5 * np.array(scales)  # Blender cube have size of 2 by default.
-        bpy.ops.object.transform_apply(
-            {"selected_editable_objects": [box]}, location=False, rotation=False, scale=True
-        )
+
+        bpy.ops.object.select_all(action="DESELECT")
+        box.select_set(True)
+        bpy.ops.object.transform_apply()
         geometry_objects.append(box)
 
     # Additional processing for all geometry objects in this visual
     for object in geometry_objects:
         if "origin" in visual:
-            set_transform_from_origin(object, visual["origin"])
+            set_delta_transform_from_origin(object, visual["origin"])
 
         object.lock_location = (True, True, True)
         object.lock_rotation = (True, True, True)
@@ -133,163 +129,179 @@ def import_visual(visual: dict, parent: bpy.types.Object, urdf_dir: str) -> None
 
 
 def import_link(link: dict, urdf_dir: str) -> bpy.types.Object:
-    empty = create_empty_for_link(link)
-
+    empty = create_locked_empty(link["@name"])
     if "visual" not in link:
         return empty
 
     # One link can contain multiple visuals.
     visuals = link["visual"]
-    if not isinstance(link["visual"], list):
-        visuals = [visuals]
-
+    visuals = visuals if isinstance(visuals, list) else [visuals]
     for visual in visuals:
         import_visual(visual, empty, urdf_dir)
-
     return empty
 
 
-def set_up_mimic_revolute_joint(
-    joint: dict,
-    mimicked_joint: dict,
-    child: bpy.types.Object,
-    child_of_mimicked_joint: bpy.types.Object,
-    axis: list[float],
-    axis_index: int,
-):
-    """URDF mimic joints are represented in Blender as copy rotation constraints on the joint's child.
+def create_locked_empty(name):
+    bpy.ops.object.empty_add(type="ARROWS", radius=0.05)
+    empty = bpy.context.object
+    empty.name = name
+    empty.lock_rotation = (True, True, True)
+    empty.lock_location = (True, True, True)
+    return empty
+
+
+def make_transform_with_z_aligned_to_axis(axis: list) -> np.ndarray:
+    """Creates a transform matrix that has its Z-axis aligned to the given axis.
+    The X and Y axes are arbitrary, but orthogonal to the Z-axis and normalized.
 
     Args:
-        joint (_type_): _description_
-        child (_type_): _description_
-        child_of_mimicked_joint (_type_): _description_
+        axis (list): The axis to align the Z-axis to. Must be a normalized 3D vector.
+
+    Returns:
+        np.ndarray: a 4x4 homogeneous transform matrix.
     """
+    Z = np.array(axis)
+    temp = np.array([1, 0, 0])
+    if np.abs(np.dot(temp, Z)) > 0.999:
+        temp = np.array([0, 1, 0])
+    X = np.cross(Z, temp)
+    X = X / np.linalg.norm(X)
+    Y = np.cross(Z, X)
+    orientation = np.column_stack([X, Y, Z])
+    transform = np.identity(4)
+    transform[:3, :3] = orientation
+    return transform
 
-    # Undo the freeing of the revolute joint (this is another reason to move the mimic function call.)
-    child.lock_rotation[axis_index] = True
-    child.empty_display_size = 0.05
 
-    multiplier = float(joint["mimic"]["@multiplier"])
+def insert_joint_axis_empty(joint: bpy.types.Object, child: bpy.types.Object, axis: list):
+    joint_axis_empty = create_locked_empty(f"{joint.name}_axis")
+    joint_axis_transform = make_transform_with_z_aligned_to_axis(axis)
 
+    joint_axis_empty.parent = joint
+    joint_axis_empty.matrix_parent_inverse = np.linalg.inv(joint_axis_transform)
+
+    # Parent the child to the joint axis empty, but keep the transform from the previous parenting
     bpy.ops.object.select_all(action="DESELECT")
     child.select_set(True)
-    bpy.context.view_layer.objects.active = child
-    bpy.ops.object.constraint_add(type="COPY_ROTATION")
-    constraint = child.constraints["Copy Rotation"]
-    constraint.target = child_of_mimicked_joint
-    constraint.mix_mode = "ADD"
-    # Set these spaces to local so that global rotations don't break the mimic.
-    constraint.target_space = "LOCAL"
-    constraint.owner_space = "LOCAL"
-
-    # Disable the copy rotation for the non-joint axes.
-    non_joint_axes = {0, 1, 2} - {axis_index}
-    for i in non_joint_axes:
-        if i == 0:
-            constraint.use_x = False
-        elif i == 1:
-            constraint.use_y = False
-        elif i == 2:
-            constraint.use_z = False
-
-    mimicked_axis = [1.0, 0.0, 0.0]  # Default axis from spec
-    if "axis" in mimicked_joint:
-        mimicked_axis = parse_vector_string(mimicked_joint["axis"]["@xyz"])
-
-    invert_axis = np.isclose(-1, multiplier * np.dot(np.array(mimicked_axis), np.array(axis)))
-    if invert_axis:
-        if axis_index == 0:
-            constraint.invert_x = True
-        elif axis_index == 1:
-            constraint.invert_y = True
-        elif axis_index == 2:
-            constraint.invert_z = True
+    joint_axis_empty.select_set(True)
+    bpy.context.view_layer.objects.active = joint_axis_empty
+    bpy.ops.object.parent_no_inverse_set(keep_transform=True)
+    return joint_axis_empty
 
 
-def set_up_relovute_joint(joint: dict, child: bpy.types.Object, joints_by_name: dict, empties: dict):
+def insert_joint(joint: dict, link_empties_by_name: dict):
+    joint_empty = create_locked_empty(joint["@name"])
+    parent = link_empties_by_name[joint["parent"]["@link"]]
+    child = link_empties_by_name[joint["child"]["@link"]]
+
+    if "origin" in joint:
+        set_delta_transform_from_origin(joint_empty, joint["origin"])
+
+    joint_empty.parent = parent
+    child.parent = joint_empty
+    return joint_empty
+
+
+def add_mimic_driver(
+    driver_joint,
+    mimic_joint,
+    multiplier: float,
+    offset: float,
+    mimic_revolute: bool = True,
+    driver_revolute: bool = True,
+):
+    mimic_property = "rotation_euler" if mimic_revolute else "location"
+    driver_property = "rotation_euler" if driver_revolute else "location"
+    driver = mimic_joint.driver_add(mimic_property, 2).driver
+    variable = driver.variables.new()
+    variable.name = "var"
+    variable.targets[0].id = driver_joint
+    variable.targets[0].data_path = f"{driver_property}.z"
+    driver.expression = f"{multiplier} * {variable.name} + {offset}"
+
+
+def configure_axis_joint(joint, joint_empty, child):
     axis = [1.0, 0.0, 0.0]  # Default axis from spec
     if "axis" in joint:
         axis = parse_vector_string(joint["axis"]["@xyz"])
+    joint_axis_empty = insert_joint_axis_empty(joint_empty, child, axis)
+    # Make the joint axis more visible
+    joint_axis_empty.empty_display_type = "SINGLE_ARROW"
+    joint_axis_empty.empty_display_size = 0.30
 
-    if np.count_nonzero(axis) > 1:
-        name = joint["@name"]
-        print(f"Ignoring joint {name}. Currently only joints with x,y or z aligned axes are supported.")
-        return
-
-    axis_index = np.where(np.array(axis) != 0)[0][0]  # Index of the non-zero axis
-
-    child.lock_rotation[axis_index] = False
-    child.empty_display_size = 0.1
-
-    # TODO consider handling this case different function, as it add two parameter to this one.
-    if "mimic" in joint:
-        mimicked_joint = joints_by_name[joint["mimic"]["@joint"]]
-        child_of_mimicked_joint = empties[mimicked_joint["child"]["@link"]]
-        set_up_mimic_revolute_joint(joint, mimicked_joint, child, child_of_mimicked_joint, axis, axis_index)
+    # Free up DoFs of the joint axis.
+    if joint["@type"] in ["revolute", "continuous"]:
+        joint_axis_empty.lock_rotation[2] = False
+    elif joint["@type"] == "prismatic":
+        joint_axis_empty.lock_location[2] = False
+    elif joint["@type"] == "planar":  # UNTESTED
+        joint_axis_empty.lock_location[0] = False
+        joint_axis_empty.lock_location[1] = False
+    return joint_axis_empty
 
 
-def set_up_prismatic_joint(joint: dict, child: bpy.types.Object):
-    # These 10 lines are currently duplicated from the revolute joint set up.
-    axis = [1.0, 0.0, 0.0]  # Default axis from spec
-    if "axis" in joint:
-        axis = parse_vector_string(joint["axis"]["@xyz"])
+def configure_mimic_joint(joint: dict, joint_empties_by_name: dict):
+    joint_name = joint["@name"]
+    mimic_joint_empty = joint_empties_by_name[joint_name]
+    if joint_name + "_axis" in joint_empties_by_name:
+        mimic_joint_empty = joint_empties_by_name[joint_name + "_axis"]
 
-    if np.count_nonzero(axis) > 1:
-        name = joint["@name"]
-        print(f"Ignoring joint {name}. Currently only joints with x,y or z aligned axes are supported.")
-        return
+    mimic_joint_empty.empty_display_size = 0.1
 
-    axis_index = np.where(np.array(axis) != 0)[0][0]  # Index of the non-zero axis
+    mimic = joint["mimic"]
+    driver_joint_name = mimic["@joint"]
+    driver_joint_empty = joint_empties_by_name[driver_joint_name]
+    if driver_joint_name + "_axis" in joint_empties_by_name:
+        driver_joint_empty = joint_empties_by_name[driver_joint_name + "_axis"]
 
-    child.lock_location[axis_index] = False
-    child.empty_display_size = 0.1
+    multiplier = 1.0 if "@multiplier" not in mimic else float(mimic["@multiplier"])
+    offset = 0.0 if "@offset" not in mimic else float(mimic["@offset"])
+    add_mimic_driver(driver_joint_empty, mimic_joint_empty, multiplier, offset)
 
 
 def import_urdf(urdf_path: str):
     urdf_dict = read_urdf_as_dictionary(urdf_path)
     urdf_dir = os.path.dirname(urdf_path)
 
+    # Importing the loose links and their visuals.
     links = urdf_dict["robot"]["link"]
-    empties = {}  # URDF links are represented as empties in Blender
+    link_empties_by_name = {}
     for link in links:
         link_empty = import_link(link, urdf_dir)
-        empties[link["@name"]] = link_empty
-        # By default, we lock all degrees of freedom of a link.
-        # Joints will be represented as unlocked degrees of freedom.
-        link_empty.lock_rotation = (True, True, True)
-        link_empty.lock_location = (True, True, True)
+        link_empties_by_name[link["@name"]] = link_empty
 
+    # Inserting joint empties between links through parenting.
     joints = urdf_dict["robot"]["joint"]
-    joints_by_name = {joint["@name"]: joint for joint in joints}
+    joint_empties_by_name = {}
     for joint in joints:
-        parent = empties[joint["parent"]["@link"]]
-        child = empties[joint["child"]["@link"]]
-        child.parent = parent
+        joint_empty = insert_joint(joint, link_empties_by_name)
+        joint_empties_by_name[joint["@name"]] = joint_empty
 
-        if "origin" in joint:
-            set_transform_from_origin(child, joint["origin"])
+    # Setting up the joint types
+    for joint in joints:
+        joint_empty = joint_empties_by_name[joint["@name"]]
+        joint_types_that_use_axis = ["revolute", "prismatic", "planar", "continuous"]
+        if joint["@type"] in joint_types_that_use_axis:
+            child = link_empties_by_name[joint["child"]["@link"]]
+            joint_axis_empty = configure_axis_joint(joint, joint_empty, child)
+            joint_empties_by_name[joint["@name"] + "_axis"] = joint_axis_empty
+        elif joint["@type"] == "floating":
+            joint_empty.lock_rotation = False, False, False
+            joint_empty.lock_location = False, False, False
 
-        joint_type = joint["@type"]
-        if joint_type == "fixed":
-            pass  # Fixed joints are already set up correctly, because we lock all DoFs by default.
-        elif joint_type == "revolute":
-            set_up_relovute_joint(joint, child, joints_by_name, empties)
-        elif joint_type == "prismatic":
-            set_up_prismatic_joint(joint, child)
-        else:
-            print(f"Ignoring joint of type {joint_type}. Not implemented yet.")
+    # Configuring the mimic joints
+    for joint in joints:
+        if "mimic" in joint:
+            configure_mimic_joint(joint, joint_empties_by_name)
 
-    # Unlock the root links so the model can be moved freely in the scene.
+    # Unlocking the root links
     child_links = [joint["child"]["@link"] for joint in joints]
     root_links = [link["@name"] for link in links if link["@name"] not in child_links]
-
-    blender_root_links = [empties[root_link] for root_link in root_links]
-
+    blender_root_links = [link_empties_by_name[root_link] for root_link in root_links]
     for root_link in blender_root_links:
         root_link.lock_location = False, False, False
         root_link.lock_rotation = False, False, False
 
-    print("root links are: ", root_links)
     return blender_root_links
 
 
@@ -298,17 +310,10 @@ if __name__ == "__main__":
 
     # UR5 and Robotiq 2F-85 gripper
     urdf_path = "/home/idlab185/urdfpy/tests/data/ur5/ur5.urdf"
+    urdf_path = "/home/idlab185/urdf-workshop/universal_robots/ros/ur10e/ur10e.urdf"
     # urdf_path = "/home/idlab185/robotiq_arg85_description/robots/robotiq_arg85_description.URDF"
     # urdf_path = "/home/idlab185/robotiq_2finger_grippers/robotiq_2f_85_gripper_visualization/urdf/robotiq2f85.urdf"
 
-    base = import_urdf(urdf_path)[0]
-    base.location = (-2, 0, 0)
-
-    # PartNet mobility samples
-    # urdf_path = "/home/idlab185/partnet-mobility-sample/44853/mobility.urdf"  # cabinet
-    # urdf_path = "/home/idlab185/partnet-mobility-sample/7128/mobility.urdf"
-    # urdf_path = "/home/idlab185/partnet-mobility-sample/7130/mobility.urdf"
-    urdf_path = "/home/idlab185/partnet-mobility-sample/103452/mobility.urdf"  # washing machine
     import_urdf(urdf_path)
 
     # Make the scene a bit prettier
