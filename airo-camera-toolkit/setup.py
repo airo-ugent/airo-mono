1c87947f32054c61a875b0320f1889bbef71bcca
tlips <thomas.lips@ugent.be>
2023-02-28 12:08:01 +0100

    fix rerun wheel dependency

--- airo-camera-toolkit/setup.py
+++ airo-camera-toolkit/setup.py
@@ -9,11 +9,11 @@ setuptools.setup(
     description="Interfaces and common functionality to work with RGB(D) cameras for robotic manipulation at the Ghent University AI and Robotics Lab",
     author="Thomas Lips",
     author_email="thomas.lips@ugent.be",
-    # TODO: refer to PyPI package for rerun as soon as the next patch is released: https://github.com/rerun-io/rerun/issues/1320
     install_requires=[
         "numpy",
         "matplotlib",
-        "https://github.com/rerun-io/rerun/releases/download/latest/rerun_sdk-0.2.0+df920dc.1-cp38-abi3-manylinux_2_31_x86_64.whl",
+        # TODO: add the PyPI package for rerun as soon as the next patch is released: https://github.com/rerun-io/rerun/issues/1320
+        "rerun-sdk @ https://github.com/rerun-io/rerun/releases/download/latest/rerun_sdk-0.2.0+df920dc.1-cp38-abi3-manylinux_2_31_x86_64.whl",
     ],
     extras_require={
         "external": [
