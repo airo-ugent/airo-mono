77236700c4aa24de05399b5b665e88be55fdaef5
tlips <thomas.lips@ugent.be>
2023-02-28 11:52:41 +0100

    add example to show how to use rerun w/ Zed

--- airo-camera-toolkit/setup.py
+++ airo-camera-toolkit/setup.py
@@ -6,11 +6,14 @@ root_folder = pathlib.Path(__file__).parents[1]
 setuptools.setup(
     name="airo_camera_toolkit",
     version="0.0.1",
-    description="Interfaces, and common functionality to work with RGB(D) cameras for robotic manipulation at the Ghent University AI and Robotics Lab",
+    description="Interfaces and common functionality to work with RGB(D) cameras for robotic manipulation at the Ghent University AI and Robotics Lab",
     author="Thomas Lips",
     author_email="thomas.lips@ugent.be",
+    # TODO: refer to PyPI package for rerun as soon as the next patch is released: https://github.com/rerun-io/rerun/issues/1320
     install_requires=[
         "numpy",
+        "matplotlib",
+        "pip install https://github.com/rerun-io/rerun/releases/download/latest/rerun_sdk-0.2.0+df920dc.1-cp38-abi3-manylinux_2_31_x86_64.whl",
     ],
     extras_require={
         "external": [
