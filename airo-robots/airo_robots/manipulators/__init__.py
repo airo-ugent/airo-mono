eb67e453cdf138c2a463caf8b3a3856644e7c4c7
Thomas Lips <37955681+tlpss@users.noreply.github.com>
2023-03-14 15:51:37 +0100

    Awaitable manipulator interface (#30)
    * use waitable actions for manipulator interface
    
    * make the default wait time action-specific
    
    so that you can tailor it upfront and the user does not have to think about it.
    
    * extend manipulator interface
    
    * implement new interface and adapt tests
    
    * rename implementation class
    
    * add setter for gripper property
    
    * cast numpy.bool_ to bool and other mypy fixes

--- airo-robots/airo_robots/manipulators/__init__.py
+++ airo-robots/airo_robots/manipulators/__init__.py
@@ -1,1 +1,2 @@
-# keep mypy from complaining..
+from airo_robots.manipulators.hardware.ur_rtde import URrtde  # noqa F401
+from airo_robots.manipulators.position_manipulator import PositionManipulator  # noqa F401
