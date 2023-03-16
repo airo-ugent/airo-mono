698242b7c077bd9ac106de5d3b0c0f88ea537219
Victorlouisdg <victorlouisdg@gmail.com>
2023-03-16 16:43:20 +0100

    Airo-dataset-tools initial version (#27)
    * Initial commit, adapted from research template
    
    * initial version of coco parser
    
    * remove unused files
    
    * update readme
    
    * make RLE type more strict and fix some typos
    
    * add package to CI
    
    * add missing init file
    
    * fix mypy errors
    
    * use typing.Dict to be python3.8 compliant
    
    * improve dataset naming
    
    * propagate renamings to tests and add init to test folder to have them auto-refactored in the future.
    
    * use typing.List
    
    * use typing.Tuple to be python 3.8 compliant
    
    * fix category type for keypoints dataset parser
    
    * add more validators
    
    * fix mypy error
    
    ---------
    
    Co-authored-by: tlips <thomas.lips@ugent.be>
    Co-authored-by: tlpss <thomas17.lips@gmail.com>

---
+++ airo-dataset-tools/test/test_coco_load.py
@@ -0,0 +1,63 @@
+"""Tests for loading datasets in COCO format.
+
+Part of the tests use annotations from the official COCO dataset.
+For this we use a small subset of the 2017 Val annotations files, the full versions can be found here:
+https://cocodataset.org/#download
+
+We do not use the captions, so we only consider these files:
+- instances_val2017.json
+- person_keypoints_val2017.json
+"""
+
+import json
+import os
+
+import pytest
+from airo_dataset_tools.coco.coco_parser import (
+    CocoCategory,
+    CocoInstancesDataset,
+    CocoKeypointCategory,
+    CocoKeypointsDataset,
+)
+
+
+def test_coco_load_instances():
+    """Test whether we can correctly load the partial instances_val2017 dataset."""
+    test_dir = os.path.dirname(os.path.realpath(__file__))
+    annotations = os.path.join(test_dir, "test_data/instances_val2017_small.json")
+
+    with open(annotations, "r") as file:
+        data = json.load(file)
+        coco_instances = CocoInstancesDataset(**data)
+        # Check a few known lengths to ensure that all elements were loaded
+        assert len(coco_instances.images) == 3
+        assert len(coco_instances.categories) == 80
+        assert len(coco_instances.annotations) == 34
+
+        assert isinstance(coco_instances.categories[0], CocoCategory)
+
+
+def test_coco_load_keypoints():
+    """Test whether we can correctly load the partial person_keypoints_val2017 dataset."""
+    test_dir = os.path.dirname(os.path.realpath(__file__))
+    annotations = os.path.join(test_dir, "test_data/person_keypoints_val2017_small.json")
+
+    with open(annotations, "r") as file:
+        data = json.load(file)
+        coco_keypoints = CocoKeypointsDataset(**data)
+        assert len(coco_keypoints.images) == 2
+        assert len(coco_keypoints.categories) == 1
+        assert len(coco_keypoints.annotations) == 2
+
+        assert isinstance(coco_keypoints.categories[0], CocoKeypointCategory)
+
+
+def test_coco_load_instances_incorrectly():
+    """Test whether an exception is raised when we try to load the regular instances as a keypoints dataset."""
+    test_dir = os.path.dirname(os.path.realpath(__file__))
+    annotations = os.path.join(test_dir, "test_data/instances_val2017_small.json")
+
+    with open(annotations, "r") as file:
+        data = json.load(file)
+        with pytest.raises(Exception):
+            CocoKeypointsDataset(**data)
