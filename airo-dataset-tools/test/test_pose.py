8a79bb0a9180259d30d40af1acdf297e7bf68a32
Victorlouisdg <victorlouisdg@gmail.com>
2023-03-17 14:27:43 +0100

    Initial version of Pose format (#35)
    * initial version of pose format
    
    * resolve PR comments

---
+++ airo-dataset-tools/test/test_pose.py
@@ -0,0 +1,27 @@
+import json
+import pathlib
+
+import numpy as np
+from airo_dataset_tools.pose import EulerAngles, Pose, Position
+
+
+def test_pose_save_and_load(tmp_path: pathlib.Path):
+    pose = Pose(
+        position_in_meters=Position(x=1.0, y=2.0, z=3.0),
+        rotation_euler_XYZ_in_radians=EulerAngles(roll=np.pi / 4, pitch=-np.pi / 2, yaw=np.pi),
+    )
+
+    with open(tmp_path / "pose.json", "w") as file:
+        json.dump(pose.dict(exclude_none=True), file, indent=4)
+
+    pose2 = Pose.parse_file(tmp_path / "pose.json")
+    assert pose2.position_in_meters.x == 1.0
+    assert pose2.position_in_meters.y == 2.0
+    assert pose2.position_in_meters.z == 3.0
+    assert pose2.rotation_euler_XYZ_in_radians.roll == np.pi / 4
+    assert pose2.rotation_euler_XYZ_in_radians.pitch == -np.pi / 2
+    assert pose2.rotation_euler_XYZ_in_radians.yaw == np.pi
+
+
+if __name__ == "__main__":
+    test_pose_save_and_load()
