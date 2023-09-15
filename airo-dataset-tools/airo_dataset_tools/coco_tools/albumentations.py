d518d674f6f63ea0e16219f9699ccf117c323e3d
tlpss <thomas17.lips@gmail.com>
2023-09-15 14:51:47 +0200

    mypy fixes for Resize

--- airo-dataset-tools/airo_dataset_tools/coco_tools/albumentations.py
+++ airo-dataset-tools/airo_dataset_tools/coco_tools/albumentations.py
@@ -1,14 +1,16 @@
+from typing import Any
+
 import albumentations as A
+import numpy as np
 from PIL import Image
-import numpy as np 
 
 
-class PillowResize(A.Resize):
+class PillowResize(A.Resize):  # type: ignore
     """Use Pillow (instead of OpenCV) to resize the input to the given height and width.
     always uses Bicubic interpolation.
 
     PIllow instead of Opencv because opencv does not adapt the filter size to the scaling factor,
-    which creates artifacts in the output for large downscaling factors. 
+    which creates artifacts in the output for large downscaling factors.
     cf. https://arxiv.org/pdf/2104.11222.pdf
 
     Args:
@@ -23,12 +25,13 @@ class PillowResize(A.Resize):
         uint8, float32
     """
 
-    def __init__(self, height, width, interpolation=Image.BICUBIC, always_apply=False, p=1):
-        super(PillowResize, self).__init__(height,width, always_apply=always_apply, p=p)
+    def __init__(
+        self, height: int, width: int, interpolation: Any = Image.BICUBIC, always_apply: bool = False, p: float = 1.0
+    ):
+        super(PillowResize, self).__init__(height, width, always_apply=always_apply, p=p)
         self.height = height
         self.width = width
         self.interpolation = interpolation
 
-    def apply(self, img, **params):
+    def apply(self, img: np.ndarray, **params: dict) -> np.ndarray:
         return np.array(Image.fromarray(img).resize((self.width, self.height), self.interpolation))
-
--- airo-dataset-tools/test/test_pillow_resize.py
+++ airo-dataset-tools/test/test_pillow_resize.py
@@ -1,9 +1,9 @@
+import numpy as np
 from airo_dataset_tools.coco_tools.albumentations import PillowResize
-import numpy as np 
-import albumentations as A
+
 
 def test_resize():
     img = np.random.randint(0, 255, (100, 100, 3), dtype=np.uint8)
-    resize = PillowResize(10,10)
-    resized_img = resize(image=img)['image']
-    assert resized_img.shape == (10,10,3)
+    resize = PillowResize(10, 10)
+    resized_img = resize(image=img)["image"]
+    assert resized_img.shape == (10, 10, 3)
