03dcf935f8bf4c13089f4a76c06e4ced0a593b78
tlips <thomas.lips@ugent.be>
2023-12-12 11:31:43 +0100

    add coco merge to CLI

--- airo-dataset-tools/airo_dataset_tools/cli.py
+++ airo-dataset-tools/airo_dataset_tools/cli.py
@@ -8,6 +8,7 @@ import click
 from airo_dataset_tools.coco_tools.change_coco_images_prefix import change_coco_json_image_prefix
 from airo_dataset_tools.coco_tools.coco_instances_to_yolo import create_yolo_dataset_from_coco_instances_dataset
 from airo_dataset_tools.coco_tools.fiftyone_viewer import view_coco_dataset
+from airo_dataset_tools.coco_tools.merge_datasets import merge_coco_datasets
 from airo_dataset_tools.coco_tools.split_dataset import split_and_save_coco_dataset
 from airo_dataset_tools.coco_tools.transform_dataset import resize_coco_dataset
 from airo_dataset_tools.cvat_labeling.convert_cvat_to_coco import cvat_image_to_coco
@@ -112,6 +113,23 @@ def change_coco_images_prefix_cli(
 ) -> None:
     """change the prefix of images in a coco dataset."""
     return change_coco_json_image_prefix(coco_json, current_prefix, new_prefix, target_json_path)
+
+
+@cli.command(name="merge-coco-datasets")
+@click.argument("coco-json-1", type=click.Path(exists=True))
+@click.argument("coco-json-2", type=click.Path(exists=True))
+@click.option(
+    "--target-json-path",
+    type=str,
+    help="optional path to save the merged dataset to. If none is provided, a new directory will be created in the parent directory of coco-json-1 ",
+)
+def merge_coco_datasets_cli(coco_json_1: str, coco_json_2: str, target_json_path: Optional[str] = None) -> None:
+    """merge two coco datasets into a single dataset."""
+    if not target_json_path:
+        target_json_path = os.path.join(os.path.dirname(coco_json_1), "merged")
+        os.makedirs(target_json_path, exist_ok=True)
+        target_json_path = os.path.join(target_json_path, "annotations.json")
+    return merge_coco_datasets(coco_json_1, coco_json_2, target_json_path)
 
 
 if __name__ == "__main__":
