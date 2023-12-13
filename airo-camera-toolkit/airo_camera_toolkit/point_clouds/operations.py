f2a4aeafb420b2237493a41eddab2286853a27fa
victorlouisdg <victorlouisdg@gmail.com>
2023-12-13 16:06:44 +0100

    finish pointcloud tutorial

--- airo-camera-toolkit/airo_camera_toolkit/point_clouds/operations.py
+++ airo-camera-toolkit/airo_camera_toolkit/point_clouds/operations.py
@@ -1,9 +1,34 @@
+import numpy as np
 from airo_typing import BoundingBox3DType, PointCloud
 
 
-def crop_pointcloud(
-    bounding_box: BoundingBox3DType,
+def filter_point_cloud(point_cloud: PointCloud, mask: np.ndarray) -> PointCloud:
+    """Creates a new point cloud that is filtered by the given mask.
+    Will also filter the colors and attributes if they are present.
+
+    Args:
+        point_cloud: the point cloud to filter
+        mask: the mask to filter the point cloud by, must be the same length as the point cloud
+
+    Returns:
+        the new filtered point cloud
+    """
+    points = point_cloud.points[mask]
+    colors = None if point_cloud.colors is None else point_cloud.colors[mask]
+
+    attributes = None
+    if point_cloud.attributes is not None:
+        attributes = {}
+        for key, value in point_cloud.attributes.items():
+            attributes[key] = value[mask]
+
+    point_cloud_filtered = PointCloud(points, colors, attributes)
+    return point_cloud_filtered
+
+
+def crop_point_cloud(
     point_cloud: PointCloud,
+    bounding_box: BoundingBox3DType,
 ) -> PointCloud:
     """Creates a new point cloud that is cropped to the given bounding box.
     Will also crop the colors and attributes if they are present.
@@ -15,26 +40,9 @@ def crop_pointcloud(
     Returns:
         the new cropped point cloud
     """
+    points = point_cloud.points
+    x, y, z = points[:, 0], points[:, 1], points[:, 2]
     (x_min, y_min, z_min), (x_max, y_max, z_max) = bounding_box
 
-    points = point_cloud.points
-
-    mask = (
-        (points[:, 0] >= x_min)
-        & (points[:, 0] <= x_max)
-        & (points[:, 1] >= y_min)
-        & (points[:, 1] <= y_max)
-        & (points[:, 2] >= z_min)
-        & (points[:, 2] <= z_max)
-    )
-    points_cropped = points[mask]
-    colors_cropped = None if point_cloud.colors is None else point_cloud.colors[mask]
-
-    attributes_cropped = None
-    if point_cloud.attributes is not None:
-        attributes_cropped = {}
-        for key, value in point_cloud.attributes.items():
-            attributes_cropped[key] = value[mask]
-
-    point_cloud_cropped = PointCloud(points_cropped, colors_cropped, attributes_cropped)
-    return point_cloud_cropped
+    crop_mask = (x >= x_min) & (x <= x_max) & (y >= y_min) & (y <= y_max) & (z >= z_min) & (z <= z_max)
+    return filter_point_cloud(point_cloud, crop_mask)
---
+++ airo-camera-toolkit/airo_camera_toolkit/point_clouds/visualization.py
@@ -0,0 +1,24 @@
+from typing import Tuple
+
+import open3d as o3d
+from airo_typing import Vector3DType
+
+
+def open3d_point(
+    position: Vector3DType, color: Tuple[float, float, float], radius: float = 0.01
+) -> o3d.geometry.TriangleMesh:
+    """Creates a small sphere mesh for visualization in open3d.
+
+    Args:
+        position:  position of the point
+        color: RGB color of the point
+        radius: radius of the sphere
+
+    Returns:
+        sphere: an open3d mesh
+    """
+    sphere = o3d.geometry.TriangleMesh.create_sphere(radius=radius)
+    sphere.translate(position)
+    sphere.paint_uniform_color(color)
+    sphere.compute_vertex_normals()
+    return sphere
