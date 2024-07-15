6dcbbcd142f222191704a8512d176f45cc21347d
Mathieu De Coster <mathieu.decoster@ugent.be>
2024-07-15 09:41:20 +0200

    Add wait() to robot testing

--- airo-robots/airo_robots/drives/hardware/manual_robot_testing.py
+++ airo-robots/airo_robots/drives/hardware/manual_robot_testing.py
@@ -5,14 +5,14 @@ from airo_robots.drives.mobile_robot import MobileRobot
 
 def manually_test_robot_implementation(robot: MobileRobot) -> None:
     input("robot will now move forward 10cm")
-    robot.set_platform_velocity_target(0.1, 0.0, 0.0, 1.0)
+    robot.set_platform_velocity_target(0.1, 0.0, 0.0, 1.0).wait()
 
     input("robot will now move left 10cm")
-    robot.set_platform_velocity_target(0.0, 0.1, 0.0, 1.0)
+    robot.set_platform_velocity_target(0.0, 0.1, 0.0, 1.0).wait()
 
     input("robot will now make two short rotations")
-    robot.set_platform_velocity_target(0.0, 0.0, 0.1, 1.0)
-    robot.set_platform_velocity_target(0.0, 0.0, -0.1, 1.0)
+    robot.set_platform_velocity_target(0.0, 0.0, 0.1, 1.0).wait()
+    robot.set_platform_velocity_target(0.0, 0.0, -0.1, 1.0).wait()
 
     input("robot will now return to original position")
-    robot.set_platform_velocity_target(-0.1, -0.1, 0.0, 1.0)
+    robot.set_platform_velocity_target(-0.1, -0.1, 0.0, 1.0).wait()
