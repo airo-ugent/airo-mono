7c295db486c5336fad705771fb8b8658c776d828
Mathieu De Coster <mth.decoster@gmail.com>
2024-10-07 15:34:02 +0200

    Update dependency for airo-tulip to 0.0.10

--- airo-robots/setup.py
+++ airo-robots/setup.py
@@ -15,7 +15,7 @@ setuptools.setup(
         "click",
         "airo-typing",
         "airo-spatial-algebra",
-        "airo-tulip==0.0.9",
+        "airo-tulip==0.0.10",
     ],
     packages=setuptools.find_packages(),
     package_data={"airo_robots": ["py.typed"]},
