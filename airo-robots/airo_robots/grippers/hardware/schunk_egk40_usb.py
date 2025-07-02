e215fa2b60a580e0b78c4b5309b014712874638c
Mathieu De Coster <mth.decoster@gmail.com>
2025-07-02 14:52:39 +0200

    Fix typing annotations in SCHUNK

--- airo-robots/airo_robots/grippers/hardware/schunk_egk40_usb.py
+++ airo-robots/airo_robots/grippers/hardware/schunk_egk40_usb.py
@@ -80,7 +80,7 @@ class SchunkEGK40_USB(ParallelPositionGripper):
         time.sleep(0.1)
 
     @property
-    def speed(self) -> float | None:
+    def speed(self) -> float:
         """return speed setting [m/s]."""
         return self.bks.set_vel / 1000
 
@@ -96,7 +96,7 @@ class SchunkEGK40_USB(ParallelPositionGripper):
         return self.bks.actual_vel / 1000
 
     @property
-    def max_grasp_force(self) -> float | None:
+    def max_grasp_force(self) -> float:
         _force = rescale_range(self.bks.set_force, 0, 100, self.gripper_specs.min_force, self.gripper_specs.max_force)
         return _force
 
@@ -171,8 +171,8 @@ class SchunkEGK40_USB(ParallelPositionGripper):
     def servo(
         self,
         width: float,
-        speed: Optional[float] = SCHUNK_DEFAULT_SPECS.min_speed,
-        force: Optional[float] = SCHUNK_DEFAULT_SPECS.min_force,
+        speed: float = SCHUNK_DEFAULT_SPECS.min_speed,
+        force: float = SCHUNK_DEFAULT_SPECS.min_force,
         set_speed_and_force: bool = True,
     ) -> AwaitableAction:
         """
@@ -200,8 +200,8 @@ class SchunkEGK40_USB(ParallelPositionGripper):
     def servo_relative(
         self,
         width_difference: float,
-        speed: Optional[float] = SCHUNK_DEFAULT_SPECS.min_speed,
-        force: Optional[float] = SCHUNK_DEFAULT_SPECS.min_force,
+        speed: float = SCHUNK_DEFAULT_SPECS.min_speed,
+        force: float = SCHUNK_DEFAULT_SPECS.min_force,
         set_speed_and_force: bool = True,
     ) -> AwaitableAction:
         """
