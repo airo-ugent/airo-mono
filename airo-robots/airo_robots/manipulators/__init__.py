7e0c71a4b08d3727c713897b255923095b15fa31
tlpss <thomas17.lips@gmail.com>
2026-07-09 16:33:40 +0200

    remove HW implementation imports from init

--- airo-robots/airo_robots/manipulators/__init__.py
+++ airo-robots/airo_robots/manipulators/__init__.py
@@ -1,3 +1,1 @@
-from airo_robots.manipulators.hardware.realman import RealmanControl  # noqa F401
-from airo_robots.manipulators.hardware.ur_rtde import URrtde  # noqa F401
 from airo_robots.manipulators.position_manipulator import PositionManipulator  # noqa F401
